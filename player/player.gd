3896cee8882216da73c78a8399f5b94e14b45e7e
Felipe Takeshi <167320163+Takexin@users.noreply.github.com>
2025-08-11 13:07:17 -0300

    fix sprite positioning

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,7 @@ const SPEED = 30000.0
 const ACCEL = 60
 
 @onready var input = $playerInput
+@onready var animation_player = $AnimatedSprite2D
 @export var player := 1:
 	set(id):
 		player = id
@@ -32,14 +33,22 @@ func _physics_process(delta: float) -> void:
 	var direction = input.direction
 	if direction.x:
 		velocity.x = move_toward(velocity.x, direction.x * SPEED * delta, ACCEL)
+		if direction.x < 0:
+			animation_player.scale.x = abs(animation_player.scale.x) * -1
+		else:
+			animation_player.scale.x = abs(animation_player.scale.x)
+		animation_player.play("lilly_run")
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, ACCEL)
-
 	if direction.y:
 		velocity.y = move_toward(velocity.y, direction.y * SPEED * delta, ACCEL)
+		animation_player.play("lilly_run")
 	else:
 		velocity.y = move_toward(velocity.y, 0, ACCEL)
-	
+	if direction == Vector2(0,0):
+		animation_player.play("lilly_idle")
+
 	#if Input.is_action_pressed("actionMouseDown"):
 		#gun_common.shoot()
 	#if Input.is_action_pressed("actionReset"):
